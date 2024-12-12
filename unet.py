c2fdb579fdea77a12b6d7e9adf5d468e2bd088d2
leo61600 <112592958+leo61600@users.noreply.github.com>
2024-12-12 09:13:39 +0800

    Add files via upload

---
+++ unet.py
@@ -0,0 +1,144 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from nets.resnet import resnet50
+from nets.vgg import VGG16
+
+class SEBlock(nn.Module):
+    def __init__(self, in_channels, reduction=16):
+        super(SEBlock, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(in_channels, in_channels // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_channels // reduction, in_channels, bias=False),
+            nn.Sigmoid()
+        )
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+
+class AttentionBlock(nn.Module):
+    def __init__(self, F_g, F_l, F_int):
+        super(AttentionBlock, self).__init__()
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, g, x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        return x * psi
+
+
+class unetUp(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(unetUp, self).__init__()
+        self.conv1 = nn.Conv2d(in_size, out_size, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_size)
+        self.conv2 = nn.Conv2d(out_size, out_size, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_size)
+        self.up = nn.UpsamplingBilinear2d(scale_factor=2)
+        self.relu = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(0.5)
+        self.attention = AttentionBlock(F_g=out_size, F_l=out_size, F_int=out_size // 2)
+
+    def forward(self, inputs1, inputs2):
+        outputs = torch.cat([inputs1, self.up(inputs2)], 1)
+        outputs = self.conv1(outputs)
+        outputs = self.bn1(outputs)
+        outputs = self.relu(outputs)
+        outputs = self.dropout(outputs)
+        outputs = self.conv2(outputs)
+        outputs = self.bn2(outputs)
+        outputs = self.relu(outputs)
+        outputs = self.dropout(outputs)
+        return outputs
+
+
+class Unet(nn.Module):
+    def __init__(self, num_classes=21, pretrained=False, backbone='vgg'):
+        super(Unet, self).__init__()
+        if backbone == 'vgg':
+            self.vgg = VGG16(pretrained=pretrained)
+            in_filters = [192, 384, 768, 1024]
+        elif backbone == "resnet50":
+            self.resnet = resnet50(pretrained=pretrained)
+            in_filters = [192, 512, 1024, 3072]
+        else:
+            raise ValueError('Unsupported backbone - `{}`, Use vgg, resnet50.'.format(backbone))
+        out_filters = [64, 128, 256, 512]
+
+        # upsampling
+        self.up_concat4 = unetUp(in_filters[3], out_filters[3])
+        self.up_concat3 = unetUp(in_filters[2], out_filters[2])
+        self.up_concat2 = unetUp(in_filters[1], out_filters[1])
+        self.up_concat1 = unetUp(in_filters[0], out_filters[0])
+
+        if backbone == 'resnet50':
+            self.up_conv = nn.Sequential(
+                nn.UpsamplingBilinear2d(scale_factor=2),
+                nn.Conv2d(out_filters[0], out_filters[0], kernel_size=3, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(out_filters[0], out_filters[0], kernel_size=3, padding=1),
+                nn.ReLU(),
+            )
+        else:
+            self.up_conv = None
+
+        self.final = nn.Conv2d(out_filters[0], num_classes, 1)
+
+        self.backbone = backbone
+
+    def forward(self, inputs):
+        if self.backbone == "vgg":
+            [feat1, feat2, feat3, feat4, feat5] = self.vgg.forward(inputs)
+        elif self.backbone == "resnet50":
+            [feat1, feat2, feat3, feat4, feat5] = self.resnet.forward(inputs)
+
+        up4 = self.up_concat4(feat4, feat5)
+        up3 = self.up_concat3(feat3, up4)
+        up2 = self.up_concat2(feat2, up3)
+        up1 = self.up_concat1(feat1, up2)
+
+        if self.up_conv != None:
+            up1 = self.up_conv(up1)
+
+        final = self.final(up1)
+
+        return final
+
+    def freeze_backbone(self):
+        if self.backbone == "vgg":
+            for param in self.vgg.parameters():
+                param.requires_grad = False
+        elif self.backbone == "resnet50":
+            for param in self.resnet.parameters():
+                param.requires_grad = False
+
+    def unfreeze_backbone(self):
+        if self.backbone == "vgg":
+            for param in self.vgg.parameters():
+                param.requires_grad = True
+        elif self.backbone == "resnet50":
+            for param in self.resnet.parameters():
+                param.requires_grad = True
